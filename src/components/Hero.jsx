7d8756e3a54deafeafda99c460f94ee01313e694
Xerra Magani <233551835+Xerra-m@users.noreply.github.com>
2026-06-26 22:29:54 +0700

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,26 @@
+import heroBg from "../assets/hero.jpg";
+
+function Hero() {
+  return (
+    <section className="relative h-[60dvh] min-h-[300px] md:h-[500px] w-full flex items-center justify-center overflow-hidden">
+      <img
+        src={heroBg}
+        alt=""
+        className="absolute inset-0 object-cover object-center w-full h-full"
+      />
+      {/* overlay */}
+      <div className="absolute inset-0 bg-black/50 backdrop-blur-xs md:backdrop-blur-sm"></div>
+      {/* teks */}
+      <div className="relative z-10 flex flex-col items-center justify-center h-full px-6 text-center max-w-4xl mx-auto">
+        <h1 className="text-slate-200 text-2xl md:text-4xl font-bold">
+          SMP N 2 Kota Tegal
+        </h1>
+        <p className="text-slate-300 text-md md:text-2xl">
+          "Sekolah Sang Juara"
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,104 @@
+import { useState } from "react";
+import {
+  Menu,
+  X,
+  House,
+  Book,
+  GraduationCap,
+  Camera,
+  Phone,
+} from "lucide-react";
+
+function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const closeSidebar = () => setIsOpen(false);
+  const navLinks = [
+    { name: "Home", href: "#", icon: House },
+    { name: "Profile", href: "#profile", icon: GraduationCap },
+    { name: "Information", href: "#info", icon: Book },
+    { name: "Galery", href: "#galery", icon: Camera },
+    { name: "Contact", href: "#contact", icon: Phone },
+  ];
+  const renderLinks = navLinks.map((link) => {
+    const Icon = link.icon;
+    return (
+      <li key={link.name} className="w-full md:w-auto">
+        <a
+          href={link.href}
+          className="text-slate-200 hover:text-white flex flex-row gap-4"
+          onClick={closeSidebar}
+        >
+          <Icon size={22} className="md:hidden" />
+          <p>{link.name}</p>
+        </a>
+      </li>
+    );
+  });
+  return (
+    <>
+      <nav className="fixed top-0 left-0 w-full z-50">
+        <div className="max-w-full mx-auto">
+          <div className="flex items-center justify-between bg-transparent px-5 py-3 md:px-10 md:py-4 text-white">
+            {/* logo */}
+            <a href="/" className="flex items-center group">
+              <img
+                src="/spenda.png"
+                alt=""
+                className="w-16 h-16 md:w-24 md:h-24"
+              />
+              <div className="flex flex-col items-left justify-center">
+                <h1 className="text-xl md:text-2xl font-bold uppercase text-slate-200">
+                  smp n 2 tegal
+                </h1>
+              </div>
+            </a>
+
+            {/* hamburger menu */}
+            <div
+              className="md:hidden flex items-center justify-center p-3 hover:scale-95 transition-transform"
+              onClick={() => setIsOpen(true)}
+            >
+              <Menu size={28} />
+            </div>
+
+            {/* desktop menu */}
+            <ul className="hidden md:flex items-center gap-4 md:text-xl md:font-bold">
+              {renderLinks}
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      {/* overlay */}
+      {isOpen && (
+        <div
+          onClick={() => setIsOpen(false)}
+          className="fixed inset-0 bg-black/50 z-40 animate-fade-in backdrop-blur-sm"
+        ></div>
+      )}
+
+      {/* sidebar */}
+      <aside
+        className={`fixed top-0 right-0 h-screen w-72 bg-[#002448] border-l-2 border-[#06386A] transition-transform duration-300 z-50 px-5 py-5 text-white ${isOpen ? "translate-x-0" : "translate-x-full"}`}
+      >
+        <div className="flex items-center justify-between border-b-2 border-slate-200">
+          <h1 className="text-2xl md:text-4xl font-bold text-slate-200">
+            Menu
+          </h1>
+
+          <div
+            onClick={() => setIsOpen(false)}
+            className="flex items-center justify-center p-3 hover:scale-95 transition-transform"
+          >
+            <X size={28} />
+          </div>
+        </div>
+        <ul className="flex flex-col p-6 gap-8 items-left font-bold text-xl">
+          {renderLinks}
+        </ul>
+      </aside>
+    </>
+  );
+}
+
+export default Navbar;
